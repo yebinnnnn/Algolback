aa0c13893ea9593de857175142d25b19e55e251d
yebinnnnn <prettybird1@naver.com>
2025-02-02 16:38:11 +0900

    modified

--- src/Silver/Back11727.java
+++ src/Silver/Back11727.java
@@ -11,6 +11,10 @@ public class Back11727 {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine());
         dp=new long[n+1];
+        if(n==1){ //n==1 이면 foundsquare 에서 접근 못함
+            System.out.println(1);
+            return;
+        }
         System.out.println(foundsquare(n));
     }
     public static long foundsquare(int n) {
@@ -18,8 +22,8 @@ public class Back11727 {
         dp[1]=1;
         dp[2]=3;
         for(int i=3; i<=n; i++){
-            dp[i]=dp[i-1]%10007+dp[i-2]*2%10007;
+            dp[i]=(dp[i-1]+dp[i-2]*2)%10007;
         }
-        return dp[n]%10007;
+        return dp[n];
     }
 }
