598e186ca97da45e80e4fcea30f07b572e94c36d
yebinnnnn <prettybird1@naver.com>
2025-04-04 16:56:06 +0900

    2504 presave

--- src/Gold/Back2504_Stack.java
+++ src/Gold/Back2504_Stack.java
@@ -7,81 +7,122 @@ import java.util.*;
 import java.io.*;
 
 //너무 하드코딩 지옥이 됐는데요
+//((())()) 이 경우만 풀면 다 품
 public class Back2504_Stack {
     static Stack<Character> stack=new Stack<>();
-    static Stack<Integer> number=new Stack<>();
-
     public static void main(String[] args) throws IOException {
-        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-        String practices= bf.readLine();
-        char[] practice=practices.toCharArray(); //char 배열로 바꿔줌.
-        for(char c : practice){
-            stack.push(c);
-        } //모든 괄호 먼저 스택에 집어넣기
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String practices = bf.readLine();
 
-        int doublecount=0;
-        int thirdcount=0;
-        int allsum=0;
-        int readysum=1;
-        StringBuilder sb= new StringBuilder();
+        int answer = 0;
+        int value = 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < practices.length(); i++) {
+            char c = practices.charAt(i);
+            if (c == '(') {
+                stack.push(c);
+                value = 2 * value;
 
-        while(!stack.isEmpty()){
-            char popped=stack.pop();
-            if(popped==')'){
-                doublecount++;
-            }
-            else if(popped==']') {
-                thirdcount++;
-            }
-            else if(popped=='('){
-                doublecount--;
-                if(doublecount<0){
+            } else if (c == '[') {
+                stack.push(c);
+                value = 3 * value;
+
+            }//여기까진 쉽당께
+            else if (c == ')') { //왼쪽이 더 할당된 경우
+                if (stack.isEmpty() || stack.peek() != '(') {
                     sb.append("0");
                     break;
+                } else if (practices.charAt(i - 1) == '(') {
+                    answer += value;
                 }
-                //축적된게 없는 상태
-                if(thirdcount==0&&doublecount<=1) {
-                    if(doublecount==0){
-                        allsum+=readysum*2;
-                        readysum=1;
-                    }
-                    else{
-                        readysum+=2;
-                    }
-                }
-                else{//축적된게 있는상태 (()[])같은 경우가 else 문에 걸리는중... 원래 의도는 ([])
-                    readysum *= 2;
-                }
-            }
-            else if(popped=='['){
-                thirdcount--;
-                if(doublecount<0){
+                stack.pop();
+                value /= 2;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.peek() != '[') {
                     sb.append("0");
                     break;
+                } else if (practices.charAt(i - 1) == '[') {
+                    answer += value;
                 }
-                if(doublecount==0&&thirdcount<=1) {
-                    if(thirdcount==0){
-                        allsum+=readysum*3;
-                        readysum=1;
-                    }
-                    else{
-                        readysum+=3;
-                    }
-                }
-                else {
-                    readysum *= 3;
-                }
+                stack.pop();
+                value /= 3;
             }
-            if(stack.isEmpty()) { //마지막에 숫자 안맞을때의 조건
-                if (doublecount > 0 || thirdcount > 0) {
-                    sb.append("0");
-                    break;
-                } else {
-                    sb.append(allsum);
-                    break;
-                }
-            }
+        }
+        if (stack.isEmpty()) {
+            sb.append("0");
+        } else {
+            sb.append(answer).append("\n");
         }
         System.out.println(sb);
     }
+
+        //스택 잘못 쌓은 예시
+//        char[] practice=practices.toCharArray(); //char 배열로 바꿔줌.
+//        for(char c : practice){
+//            stack.push(c);
+//        } //모든 괄호 먼저 스택에 집어넣기
+//
+//        int doublecount=0;
+//        int thirdcount=0;
+//        int allsum=0;
+//        int readysum=1;
+//        StringBuilder sb= new StringBuilder();
+//
+//        while(!stack.isEmpty()){
+//            char popped=stack.pop();
+//            if(popped==')'){
+//                doublecount++;
+//            }
+//            else if(popped==']') {
+//                thirdcount++;
+//            }
+//            else if(popped=='('){
+//                doublecount--;
+//                if(doublecount<0){
+//                    sb.append("0");
+//                    break;
+//                }
+//                //축적된게 없는 상태
+//                if(thirdcount==0&&doublecount<=1) {
+//                    if(doublecount==0){
+//                        allsum+=readysum*2;
+//                        readysum=1;
+//                    }
+//                    else{
+//                        readysum+=2;
+//                    }
+//                }
+//                else{//축적된게 있는상태 (()[])같은 경우가 else 문에 걸리는중... 원래 의도는 ([])
+//                    readysum *= 2;
+//                }
+//            }
+//            else if(popped=='['){
+//                thirdcount--;
+//                if(doublecount<0){
+//                    sb.append("0");
+//                    break;
+//                }
+//                if(doublecount==0&&thirdcount<=1) {
+//                    if(thirdcount==0){
+//                        allsum+=readysum*3;
+//                        readysum=1;
+//                    }
+//                    else{
+//                        readysum+=3;
+//                    }
+//                }
+//                else {
+//                    readysum *= 3;
+//                }
+//            }
+//            if(stack.isEmpty()) { //마지막에 숫자 안맞을때의 조건
+//                if (doublecount > 0 || thirdcount > 0) {
+//                    sb.append("0");
+//                    break;
+//                } else {
+//                    sb.append(allsum);
+//                    break;
+//                }
+//            }
+//        }
 }
