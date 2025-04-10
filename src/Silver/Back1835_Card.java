540c47466e8dc5c32c5dbe4939812898c1968334
yebinnnnn <prettybird1@naver.com>
2025-04-10 17:28:43 +0900

    0410 move

---
+++ src/Silver/Back1835_Card.java
@@ -0,0 +1,13 @@
+package Silver;
+
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+
+public class Back1835_Card {
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        int N= Integer.parseInt(bf.readLine());
+        Queue<Integer> q= new ArrayDeque<>();
+    }
+}
