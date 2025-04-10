4b9ce2b23b3115fb5e5bbd6e89c9e13b360925c8
yebinnnnn <prettybird1@naver.com>
2025-04-10 22:56:38 +0900

    deque card

--- src/Silver/Back1835_Card.java
+++ src/Silver/Back1835_Card.java
@@ -8,6 +8,16 @@ public class Back1835_Card {
     public static void main(String[] args)throws IOException {
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         int N= Integer.parseInt(bf.readLine());
-        Queue<Integer> q= new ArrayDeque<>();
+        Deque<Integer> deq= new ArrayDeque<>();
+        for(int i=N; i>0; i--){ //가장 큰 수부터 카운트
+            deq.addFirst(i);
+            for(int j=0; j<i; j++ ){
+                deq.addFirst(deq.pollLast());
+            }
+        }
+
+        while(!deq.isEmpty()){
+            System.out.print(deq.poll()+" ");
+        }
     }
 }
