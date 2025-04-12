5f5706019c40597566de7da0073d3319e5edbbd3
yebinnnnn <prettybird1@naver.com>
2025-04-12 20:45:38 +0900

    신입사원 greedy. 못푼 문제 복습

--- src/Silver/Back1946_regreedy.java
+++ src/Silver/Back1946_regreedy.java
@@ -3,29 +3,36 @@ package Silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Back1946_regreedy {
     public static void main(String[] args) throws IOException {
     BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
     int testcase= Integer.parseInt(bf.readLine());
     StringBuilder sb= new StringBuilder();
-    int[] resume;
-    int[] interview;
         while(testcase-->0){
             int person=Integer.parseInt(bf.readLine());
-            resume= new int[person+1];
-            interview= new int[person+1];
-            for(int i=1; i<=person; i++){
-                st=new StringTokenizer(bf.readLine());
-                resume[Integer.parseInt(st.nextToken())]=i;
-                interview[Integer.parseInt(st.nextToken())]=i;
+            int[][] apply=new int[person][2];
+            for(int i=0; i<person; i++) {
+                StringTokenizer st = new StringTokenizer(bf.readLine());
+                apply[i][0] = Integer.parseInt(st.nextToken()); // 서류
+                apply[i][1] = Integer.parseInt(st.nextToken()); // 면접
+            }
+            Arrays.sort(apply, Comparator.comparingInt(a->a[0]));
+            int count=1;
+            int interview=apply[0][1];
+
+            for(int i=1; i<person; i++) {
+                if(apply[i][1]<interview) {
+                    count++;
+                    interview=apply[i][1];
+                }
             }
 
-            for(int i=1; i<=person; i++){
-                if(res)
-            }
+            sb.append(count).append('\n');
         }
+        System.out.println(sb);
     }
 }
