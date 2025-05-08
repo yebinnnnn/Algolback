528241b35efe61504302236b787e3a46d8b2afa8
yebinnnnn <prettybird1@naver.com>
2025-05-08 19:28:01 +0900

    배낭문제 하는중

---
+++ src/Gold/Back12865_DP.java
@@ -0,0 +1,48 @@
+package Gold;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Back12865_DP {//물품의 수는 100, 무게는 십만, 가치는 천
+    public static int[][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(bf.readLine());
+        int num=Integer.parseInt(st.nextToken());
+        int weight=Integer.parseInt(st.nextToken());//버틸 수 있는 무게
+        int[][] bag=new int[num][2];
+        dp=new int[num+1][weight+1];
+        for(int i=0; i<num; i++){
+            st=new StringTokenizer(bf.readLine());
+            //0이 weight
+            bag[i][0]=Integer.parseInt(st.nextToken());
+            //1이 value
+            bag[i][1]=Integer.parseInt(st.nextToken());
+        }
+        //dp 표 형태
+        //
+
+        //주어진 가방들
+        for(int i=1;i<num;i++){
+            for(int j=1;j<=weight;j++){
+                int thisweight=bag[i][0];
+                int thisvalue=bag[i][1];
+                if(thisweight<weight){
+                    if(thisvalue+dp[i][]>dp[i-1][]){
+
+                    }else{
+
+                    }
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        System.out.println(dp[num][weight]);
+    }
+}
