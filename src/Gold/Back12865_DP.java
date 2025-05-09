9cd2d75674e4af180dd858fb021cad562b4153e3
yebinnnnn <prettybird1@naver.com>
2025-05-09 10:53:20 +0900

    배낭 알고리즘 문제

--- src/Gold/Back12865_DP.java
+++ src/Gold/Back12865_DP.java
@@ -9,37 +9,31 @@ import java.util.StringTokenizer;
 
 public class Back12865_DP {//물품의 수는 100, 무게는 십만, 가치는 천
     public static int[][] dp;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(bf.readLine());
-        int num=Integer.parseInt(st.nextToken());
-        int weight=Integer.parseInt(st.nextToken());//버틸 수 있는 무게
-        int[][] bag=new int[num][2];
-        dp=new int[num+1][weight+1];
-        for(int i=0; i<num; i++){
-            st=new StringTokenizer(bf.readLine());
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int num = Integer.parseInt(st.nextToken());
+        int weight = Integer.parseInt(st.nextToken());//버틸 수 있는 무게
+        int[][] bag = new int[num+1][2];
+        dp = new int[num + 1][weight + 1];
+        for (int i = 1; i <= num; i++) {
+            st = new StringTokenizer(bf.readLine());
             //0이 weight
-            bag[i][0]=Integer.parseInt(st.nextToken());
+            bag[i][0] = Integer.parseInt(st.nextToken());
             //1이 value
-            bag[i][1]=Integer.parseInt(st.nextToken());
+            bag[i][1] = Integer.parseInt(st.nextToken());
         }
-        //dp 표 형태
-        //
-
         //주어진 가방들
-        for(int i=1;i<num;i++){
-            for(int j=1;j<=weight;j++){
-                int thisweight=bag[i][0];
-                int thisvalue=bag[i][1];
-                if(thisweight<weight){
-                    if(thisvalue+dp[i][]>dp[i-1][]){
-
-                    }else{
-
-                    }
-                }
-                else{
-                    dp[i][j]=dp[i-1][j];
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= weight; j++) {
+                int thisweight = bag[i][0];
+                int thisvalue = bag[i][1];
+                if (thisweight <= j) {
+                    //지금 가치에 현재 책정 무게에서 담을 예정인 무게 뺀 dp의 값
+                    dp[i][j] = Math.max(dp[i - 1][j], thisvalue + dp[i - 1][j - thisweight]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
                 }
             }
         }
