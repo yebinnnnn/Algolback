03baafeed9c0df88b2e27604c9c6ccab935b3b4f
yebinnnnn <prettybird1@naver.com>
2025-05-13 13:50:46 +0900

    눡합 사과나무. 2중배열 누적합에 대해

--- src/Gold/Back20002_prefix.java
+++ src/Gold/Back20002_prefix.java
@@ -6,19 +6,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 //사각 경로로만 사과를 딸 수 있음
+//2차원 누적합, 올탐색 문제
 public class Back20002_prefix {
     public static void main(String[] args) throws IOException {
         BufferedReader bf= new BufferedReader(new InputStreamReader(System.in));
         int num=Integer.parseInt(bf.readLine());
-        int[][] map=new int[num][num];
-        int[][] pre=new int[num][num];
-        //밭에 점수 배정
-        for(int i=0; i<num; i++){
+        int[][] map=new int[num+1][num+1];
+        int[][] pre=new int[num+1][num+1];
+        int max= Integer.MIN_VALUE;
+        //밭에 점수 배정하면서 누적합 배정
+        for(int i=1; i<=num; i++){
             StringTokenizer st= new StringTokenizer(bf.readLine());
-            for(int j=0; j<num; j++){
+            for(int j=1; j<=num; j++){
                 map[i][j]=Integer.parseInt(st.nextToken());
+                //2차원방식 누적합 저장
+                pre[i][j] = pre[i-1][j]+pre[i][j-1]-pre[i-1][j-1] + map[i][j];
             }
         }
-
+        for(int i=1; i<=num; i++){ //0~부터 num까지칸의 사이즈
+            for(int j=i; j<=num; j++){ //엔드포인트 세로 좌표
+                for(int k=i; k<=num; k++){ //엔드포인트 가로 좌표
+                    int thisnum=pre[j][k]-pre[j-i][k]-pre[j][k-i]+pre[j-i][k-i];
+                    max=Math.max(max,thisnum);
+                }
+            }
+        }
+        System.out.println(max);
     }
 }
