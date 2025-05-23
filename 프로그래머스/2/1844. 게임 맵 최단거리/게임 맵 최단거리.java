a54c67b0335c2abebb44243490396839982ff37e
Kong <114918259+yebinnnnn@users.noreply.github.com>
2025-05-23 14:33:05 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.81 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    int[] X={-1,1,0,0};
+    int[] Y={0,0,-1,1};
+    int[][] distance;
+    boolean[][] checking;
+    public int solution(int[][] maps) {
+        int answer = 0;
+        int sizen=maps.length;
+        int sizem=maps[0].length;
+        distance= new int[sizen][sizem];
+        distance[0][0]=1;//초기거리 1으로 설정
+        checking= new boolean[sizen][sizem];
+        answer=bfs(sizen,sizem, maps);
+        return answer;
+    }
+    public int bfs(int n,int m, int[][] maps){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0,0});
+        while(!q.isEmpty()){
+            int[] current=q.poll();
+            int nX=current[0];
+            int nY=current[1];
+            for(int i=0; i<4; i++){
+                int dx=nX+X[i];
+                int dy=nY+Y[i];
+                if(dx>=0&&dy>=0&&dx<n&&dy<m&&!checking[dx][dy]){
+                    if(maps[dx][dy]==1){
+                        q.add(new int[]{dx,dy});
+                        distance[dx][dy]=distance[nX][nY]+1; //새 거리값도 저장
+                        checking[dx][dy]=true;
+                    }
+                }
+            }
+        }
+        return distance[n-1][m-1]==0? -1: distance[n-1][m-1];
+    }
+}
