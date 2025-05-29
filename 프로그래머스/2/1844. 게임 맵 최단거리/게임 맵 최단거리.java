f5ff9048fc820a978211a7a3c02b31d1356cba46
Kong <114918259+yebinnnnn@users.noreply.github.com>
2025-05-29 16:20:33 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.36 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
+class Solution2{
     int[] X={-1,1,0,0};
     int[] Y={0,0,-1,1};
     int[][] distance;
@@ -36,5 +36,38 @@ class Solution {
             }
         }
         return distance[n-1][m-1]==0? -1: distance[n-1][m-1];
+    }
+}
+class Solution {
+    public int[] X={-1,1,0,0};
+    public int[] Y={0,0,-1,1};
+    public int solution (int[][] map){
+        boolean[][] checking= new boolean[map.length][map[0].length];
+        int[][] depth=new int[map.length][map[0].length];
+        int answer=bfs(map, checking, 1, map.length, map[0].length, depth);
+        return answer;
+    }
+    public int bfs(int[][] map, boolean[][] checking , int start, int n, int m, int[][] depth){
+        Queue<int[]> q=new ArrayDeque<>();
+        q.add(new int[]{0,0});
+        depth[0][0]=1;
+        while(!q.isEmpty()){
+            int[] current=q.poll();
+            int nx=current[0];
+            int ny=current[1];
+            for(int i=0; i<4; i++){
+                int dx=nx+X[i];
+                int dy=ny+Y[i];
+                if(dx>=0&&dy>=0&&dx<n&&dy<m&&!checking[dx][dy]){
+                    checking[dx][dy]=true;
+                    if(map[dx][dy]==1){
+                        q.add(new int[]{dx,dy});
+                        depth[dx][dy]=depth[nx][ny]+1;
+                    }
+                }
+            }
+        }
+        return depth[n-1][m-1]==0? -1: depth[n-1][m-1];
+        
     }
 }
